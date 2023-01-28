37dfac234462fa1c6747aaf0d5405dbebc4fb574
Dave Risney <dave@deletethis.net>
2023-01-27 17:44:01 -0800

    Updating 11ty metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	"title": "Eleventy Base Blog v8",
-	"url": "https://example.com/",
+	"title": "david-risney/blog",
+	"url": "https://david-risney.github.io/blog/",
 	"language": "en",
-	"description": "I am writing about my experiences as a naval navel-gazer.",
+	"description": "David Risney's blog on web or coding things",
 	"author": {
-		"name": "Your Name Here",
-		"email": "youremailaddress@example.com",
-		"url": "https://example.com/about-me/"
+		"name": "Dave Risney",
+		"email": "david.risney+blog@gmail.com",
+		"url": "https://deletethis.net/dave/"
 	}
 }
