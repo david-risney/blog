d9de3d4a0a583624e37b65efad4ea3ad767213d0
david-risney <david-risney@users.noreply.github.com>
2025-02-07 06:15:58 +0000

    deploy: 41f83b8eb653d8cb2204306dfa9fe6a2b9216ca1

--- dave/emfed-mod.mjs
+++ dave/emfed-mod.mjs
@@ -1,5 +1,7 @@
 /* esm.sh - esbuild bundle(emfed@1.5.0) esnext production */
-async function l(e,t,n,a){let s=new URL(e),o=t??await(async()=>{let c=/@(\w+)$/.exec(s.pathname);if(!c)throw"not a Mastodon user URL";let h=c[1],g=Object.assign(new URL(s),{pathname:"/api/v1/accounts/lookup",search:`?acct=${h}`});return(await(await fetch(g)).json()).id})(),f=Object.assign(new URL(s),{pathname:`/api/v1/accounts/${o}/statuses`,search:`?limit=${n??5}&exclude_replies=${!!a}`});return await(await fetch(f)).json()}async function i(e,t,n,a){let s=[];if(a===!1){let o=Object.assign(new URL(e),{pathname:`/api/v1/statuses/${t}`});s=[await(await fetch(o)).json()]}if(n===!1){let o=Object.assign(new URL(e),{pathname:`/api/v1/statuses/${t}/context`});s=[...s,...(await(await fetch(o)).json()).descendants]}return s}import w from"./dompurify.mjs";function u(e){return Object.assign(new String(e),{__safe:null})}function p(e){return typeof e>"u"||e===null?"":typeof e=="string"||e instanceof String?e.hasOwnProperty("__safe")?e:e.replaceAll("&","&amp;").replaceAll("<","&lt;").replaceAll(">","&gt;").replaceAll('"',"&quot;").replaceAll("'","&#039;"):e.map(p).join("")}function r(e,...t){let n=e[0];for(let a=1;a<e.length;++a)n+=p(t[a-1]),n+=e[a];return u(n)}function d(e){let t=null;e.reblog&&(t={avatar:e.account.avatar,username:e.account.username,display_name:e.account.display_name,user_url:e.account.url},e=e.reblog);let n=new Date(e.created_at).toLocaleString(),a=e.media_attachments.filter(s=>s.type==="image");return r`<li class="toot postlist-item" data-avatar="${e.account.avatar}"><img class="avatar" src="${e.account.avatar}" /><a class="user postlist-link" href="${e.url}">Post by <span class="display-name">${e.account.display_name}</span>
+async function l(e,t,n,a){let s=new URL(e),o=t??await(async()=>{let c=/@(\w+)$/.exec(s.pathname);if(!c)throw"not a Mastodon user URL";let h=c[1],g=Object.assign(new URL(s),{pathname:"/api/v1/accounts/lookup",search:`?acct=${h}`});return(await(await fetch(g)).json()).id})(),f=Object.assign(new URL(s),{pathname:`/api/v1/accounts/${o}/statuses`,search:`?limit=${n??5}&exclude_replies=${!!a}`});return await(await fetch(f)).json()}async function i(e,t,n,a){let s=[];if(a===!1){let o=Object.assign(new URL(e),{pathname:`/api/v1/statuses/${t}`});s=[await(await fetch(o)).json()]}if(n===!1){let o=Object.assign(new URL(e),{pathname:`/api/v1/statuses/${t}/context`});s=[...s,...(await(await fetch(o)).json()).descendants]}return s}import w from"./dompurify.mjs";function u(e){return Object.assign(new String(e),{__safe:null})}function p(e){return typeof e>"u"||e===null?"":typeof e=="string"||e instanceof String?e.hasOwnProperty("__safe")?e:e.replaceAll("&","&amp;").replaceAll("<","&lt;").replaceAll(">","&gt;").replaceAll('"',"&quot;").replaceAll("'","&#039;"):e.map(p).join("")}function r(e,...t){let n=e[0];for(let a=1;a<e.length;++a)n+=p(t[a-1]),n+=e[a];return u(n)}function d(e){let t=null;e.reblog&&(t={avatar:e.account.avatar,username:e.account.username,display_name:e.account.display_name,user_url:e.account.url},e=e.reblog);
+let n=new Date(e.created_at).toLocaleString(),a=e.media_attachments.filter(s=>s.type==="image"),gifvs=e.media_attachments.filter(s=>s.type==="gifv"),videos=e.media_attachments.filter(s=>s.type==="video");
+return r`<li class="toot postlist-item" data-avatar="${e.account.avatar}"><img class="avatar" src="${e.account.avatar}" /><a class="user postlist-link" href="${e.url}">Post by <span class="display-name">${e.account.display_name}</span>
       <span class="username">@${e.account.username}</span>
     </a>
     <time class="postlist-date" datetime="${e.created_at}">${n}</time>
@@ -13,6 +15,30 @@ async function l(e,t,n,a){let s=new URL(e),o=t??await(async()=>{let c=/@(\w+)$/.
           <img
             class="attachment"
             src="${s.preview_url}"
+            alt="${s.description}"
+          />
+        </a>`)}
+    ${gifvs.map(s=>r` <a
+          class="attachment"
+          href="${s.url}"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <video autoplay loop muted playsinline
+            class="attachment"
+            src="${s.url}"
+            alt="${s.description}"
+          />
+        </a>`)}
+    ${videos.map(s=>r` <a
+          class="attachment"
+          href="${s.url}"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <video controls
+            class="attachment"
+            src="${s.url}"
             alt="${s.description}"
           />
         </a>`)}
